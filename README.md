1055a73d025173b1d7cb593699a6ce37e0006a1e
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2025-09-01 19:47:10 +0300

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+# Сто собк каждому
+Программа загружает сто картинок с собаками с сайта TheDogAPI, складывает эти файлы в одном месте и выводит список названий сохранённых файлов в терминал. 
+
+## Установка
+- Создать виртуальное окружение
+
+Windows
+
+```bash
+python -m venv venv
+```
+
+Linux, OSX
+
+```bash
+python3 -m venv venv
+```
+
+- Активировать окружение
+
+Windows
+
+```bash
+source ./venv/Scripts/activate
+```
+
+Linux, OSX
+
+```bash
+source ./venv/bin/activate
+```
+
+- Установить зависимости
+
+```bash
+pip install -r requirements.txt
+```
+
+## Запуск программы 
+
+```bash
+python download_dog.py
+```
---
+++ download_dog.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import requests
+
+BASE_DIR = Path(__file__).parent
+URL = 'https://api.thedogapi.com/v1/images/search'
+DOGS_DIR = DOGS_DIR = BASE_DIR / 'dogs'
+
+
+def get_new_image_url():
+    response = requests.get(URL).json()
+    random_dog = response[0].get('url')
+    return random_dog
+
+
+def download_file(url):
+    filename = url.split('/')[-1]
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка, что запрос выполнен успешно
+    with open(DOGS_DIR / filename, 'wb') as file:
+        file.write(response.content)
+
+
+def download_new_dog_image():
+    url = get_new_image_url()
+    download_file(url)
+
+
+def create_dir(dir_name): 
+    os.makedirs(dir_name, exist_ok=True)
+
+
+def list_dir(dir_name):
+    print(
+        *os.listdir(dir_name),
+        sep='\n'
+    )
+
+
+def main():
+    create_dir(DOGS_DIR)
+    for _ in range(100):
+        download_new_dog_image()
+
+
+if __name__ == '__main__':
+    start_time = datetime.now()
+    main()
+    end_time = datetime.now()
+    print(f'Время выполнения программы: {end_time - start_time}.')
+    list_dir(DOGS_DIR)
